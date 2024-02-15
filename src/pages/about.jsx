0cbf55c03e69cdb705adf8d950a93b8d8eb1ab67
AbenezerA <abitad2015@gmail.com>
2024-02-15 13:57:51 -0500

    adjusted pages for deployment for eesa feast

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,14 @@
+import { NavLink } from 'react-router-dom';
+import "./about.css";
+
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page</p>
-    </div>
+    <main class="About">
+      <h1>About Us</h1>
+      <h3>This page is currently under construction🏗️.</h3>
+      <h4>Come back soon to learn about us at CTP!</h4>
+      <h5>Click <NavLink to="/"><u>here</u></NavLink> to return to our home page.</h5>
+    </main>
   );
 }
 

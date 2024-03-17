95b3a20ac25c5378225ba63cd910f2f60d0a194e
AbenezerA <abitad2015@gmail.com>
2024-03-17 18:01:39 -0400

    tweaked homepage layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,8 +7,9 @@ import InfoSessionImage from '../assets/info-session.png';
 
 function Home() {
     useEffect(() => {
-        const elements = document.querySelectorAll('.fade-in');
 
+        // Fade-in text elements upon scrolling
+        const elements = document.querySelectorAll('.fade-in');
         function fadeinOnScroll() {
             elements.forEach(element => {
             if (!(element.classList.contains('visible')) && isInViewport(element)) {
@@ -18,6 +19,7 @@ function Home() {
             });
         };
 
+        // Helper for fadeinOnScroll; determines if element is within user view
         function isInViewport(element) {
             const rect = element.getBoundingClientRect();
             const fadeinPoint = 150;
@@ -57,7 +59,7 @@ function Home() {
                 </div>
                 <div className='col-12 col-md-7 p-5 bg-custom-blue home-section-para-container'>
                     <div className='row mb-5 section-para fade-in'>
-                        <h2 className="crimson-text">Group Mentorship</h2>
+                        <h2 className="p-0">Group Mentorship</h2>
                         <p className='p-0'>
                             Each summer, our team of volunteers consisting of current college students in the US and other countries hold <b>in-person</b> group mentorship sessions to help students navigate the college application process.
                             These sessions are <b>free</b> and run between July and August, twice a week for about <b>5 weeks</b>. Location and times vary year to year.
@@ -67,7 +69,7 @@ function Home() {
                         </div>
                     </div>
                     <div className='row mb-5 section-para fade-in'>
-                        <h3 className="crimson-text">One-on-one Mentorship</h3>
+                        <h2 className="p-0">One-on-one Mentorship</h2>
                         <p className='p-0'>
                             During the school year, our mentors are paired up with students to provide <b>virtual, one-on-one</b> mentorship. Mentors provide essay feedback, assist with SAT prep, and answer general college application questions of their mentees.
                             Mentees are chosen from the summer program through the recommendation of their group mentors.
@@ -77,7 +79,7 @@ function Home() {
                         </div>
                     </div>
                     <div className='row mb-5 section-para fade-in'>
-                        <h3 className="crimson-text">Information Sessions</h3>
+                        <h2 className="p-0">Information Sessions</h2>
                         <p className='p-0'>
                             We also hold <b>virtual</b> information sessions in the Spring where students can bring their questions on college application to recently-admitted students. 
                             These sessions are typically held in early April for two days. Topics covered include navigating the Common App, financial aid, and standardized testing.
